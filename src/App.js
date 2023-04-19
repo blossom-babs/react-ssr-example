dcdb0e94e5e8fd8c9cee29bd2619130d493b2590
Blossom <saphashb@gmail.com>
2023-04-19 13:36:51 +0100

    data cleanup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Link, BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { About, Articles, Home } from './pages';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <Router>
+      <h1> Server side Rendering Example</h1>
+      <ul>
+        <li> <Link to="/"> Home</Link></li>
+        <li> <Link to="/about"> About</Link></li>
+        <li> <Link to="/articles"> Articles</Link></li>
+      </ul>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/articles" element={<Articles />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
